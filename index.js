bd31fac5343c7dcf17510e957e9be27a5a733926
phurinat <phurinaix@gmail.com>
2019-05-26 02:56:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const upload = multer({
+    dest: "file/"
+});
+const port = process.env.PORT || 8000;
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('verification server');
+});
+
+app.post("/import", upload.single("file"), (req, res, next) => {
+    backURL = req.header('Referer') || '/';
+    if (req.file) {
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, `./file/cert.json`);
+        fs.rename(tempPath, targetPath, err => {
+            if (err) throw err;
+            res.redirect(backURL);
+        });
+    } else {
+        res.redirect(`${backURL}?status=empty`);
+    }
+});
+
+app.get("/cert", (req, res) => {
+    res.sendFile(__dirname + "/file/cert.json");
+});
+
+app.listen(port, () => {
+    console.log(`server starting on port ${port}`);
+});
